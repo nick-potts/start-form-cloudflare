e40bd0bd32256fa7ab4745237557c6a636c52128
Nick Potts <github@nickpotts.com.au>
2025-09-24 21:18:06 +0200

    remove about

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,28 +1,28 @@
 /// <reference types="vite/client" />
-import * as React from 'react'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import * as React from "react";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import {
   HeadContent,
   Link,
   Outlet,
   Scripts,
   createRootRoute,
-} from '@tanstack/react-router'
-import appCss from '~/styles/app.css?url'
+} from "@tanstack/react-router";
+import appCss from "~/styles/app.css?url";
 
 export const Route = createRootRoute({
   head: () => ({
-    links: [{ rel: 'stylesheet', href: appCss }],
+    links: [{ rel: "stylesheet", href: appCss }],
   }),
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <RootDocument>
       <Outlet />
     </RootDocument>
-  )
+  );
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
@@ -34,7 +34,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body>
         <div>
           <Link to="/">Index</Link>
-          <Link to="/about">About</Link>
         </div>
 
         {children}
@@ -42,5 +41,5 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
